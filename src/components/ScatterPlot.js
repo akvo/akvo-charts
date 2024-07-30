0d898ff7680f808684d57828338af2fb1e22c19e
ifirmawan <firmawaneiwan@gmail.com>
2024-07-30 21:58:29 +0700

    [#26] Refactoring ScatterPlot to support unit test

--- src/components/ScatterPlot.js
+++ src/components/ScatterPlot.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import { useECharts } from '../hooks';
+import React, {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef
+} from 'react';
+import * as echarts from 'echarts';
+import transformConfig from '../utils/transformConfig';
+
 import styles from '../styles.module.css';
 
 const getOptions = ({ data = [], symbolSize = 20 }) => {
@@ -14,21 +21,52 @@ const getOptions = ({ data = [], symbolSize = 20 }) => {
   };
 };
 
-const ScatterPlot = ({ config, data, symbolSize = 20 }) => {
-  const chartRef = useECharts({
-    config,
-    data,
-    getOptions: ({ data }) => getOptions({ data, symbolSize })
-  });
+const ScatterPlot = forwardRef(
+  ({ config, data, symbolSize = 20, isTest = false }, ref) => {
+    const chartRef = useRef(null);
 
-  return (
-    <div
-      ref={chartRef}
-      role="figure"
-      className={styles.container}
-      data-testid="scatter-plot"
-    />
-  );
-};
+    useImperativeHandle(ref, () => ({
+      getChartInstance: () => {
+        return chartRef.current
+          ? echarts.getInstanceByDom(chartRef.current)
+          : null;
+      }
+    }));
+
+    useEffect(() => {
+      let chart;
+
+      if (chartRef.current) {
+        chart = isTest
+          ? echarts.init(chartRef.current, 'light', {
+              renderer: 'svg',
+              width: 400,
+              height: 400
+            })
+          : echarts.init(chartRef.current, 'light', { renderer: 'svg' });
+        const options = {
+          ...transformConfig({ ...config, symbolSize }),
+          ...getOptions({ data })
+        };
+        chart.setOption(options);
+      }
+
+      return () => {
+        if (chart) {
+          chart.dispose();
+        }
+      };
+    }, [config, data, isTest, symbolSize]);
+
+    return (
+      <div
+        ref={chartRef}
+        role="figure"
+        className={styles.container}
+        data-testid="scatter-plot"
+      />
+    );
+  }
+);
 
 export default ScatterPlot;
