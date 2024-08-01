61cdd0e4590da803f1bc4a96a51b7be70e4687b6
wayangalihpratama <wgprtm@gmail.com>
2024-08-01 13:01:20 +0800

    [#28] Create StackLine chart component

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ export { default as Pie } from './components/Pie';
 export { default as Doughnut } from './components/Doughnut';
 export { default as StackBar } from './components/StackBar';
 export { default as StackClusterColumn } from './components/StackClusterColumn';
+export { default as StackLine } from './components/StackLine';
