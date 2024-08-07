164529654953a7b8873d9e7ba0512a2acf945126
ifirmawan <firmawaneiwan@gmail.com>
2024-08-07 17:04:04 +0700

    [#38] Create separate map components: GeoJson

---
+++ src/components/Map/GeoJson.js
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+import L from 'leaflet';
+
+import { useLeaflet } from '../../context/LeafletProvider';
+
+const GeoJson = ({ onClick, onMouseOver, data = {}, style = {} }) => {
+  const mapRef = useLeaflet();
+  useEffect(() => {
+    if (mapRef.current && data?.type && data?.type !== 'Topology') {
+      try {
+        L.geoJSON(data, {
+          style: () => ({
+            ...style,
+            fillOpacity: parseFloat(style?.fillOpacity || 0.2, 10)
+          }),
+          onEachFeature: (_, layer) => {
+            if (typeof onClick === 'function') {
+              layer.on({
+                click: (props) => onClick(props)
+              });
+            }
+
+            if (typeof onMouseOver === 'function') {
+              layer.on({
+                mouseover: (props) => onMouseOver(props)
+              });
+            }
+          }
+        }).addTo(mapRef.current);
+      } catch (er) {
+        console.error('GeoJson', err);
+      }
+    }
+  }, [mapRef, data, style, onClick, onMouseOver]);
+
+  return null;
+};
+
+export default GeoJson;
