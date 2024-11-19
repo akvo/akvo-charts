ae5d0bb98c92419af615cb2053d2936184ede384
ifirmawan <firmawaneiwan@gmail.com>
2024-11-19 17:24:15 +0700

    [#49] Create fnMarker & handle MarkerIcon props

--- src/components/Map/Utils.js
+++ src/components/Map/Utils.js
@@ -2,19 +2,23 @@ import L from 'leaflet';
 import mIcon from 'leaflet/dist/images/marker-icon.png';
 import mShadow from 'leaflet/dist/images/marker-shadow.png';
 import * as topojson from 'topojson-client';
+import { renderReactToDiv } from '../../utils/mapHelper';
 
-export const MarkerIcon = () => {
+export const MarkerIcon = (props = {}) => {
   const defaultIcon = typeof mIcon === 'object' ? mIcon?.src : mIcon;
   const defaultShadow = typeof mShadow === 'object' ? mShadow?.src : mShadow;
 
-  const Icon = L.icon({
-    iconUrl: defaultIcon,
-    shadowUrl: defaultShadow,
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-  });
+  const Icon = props?.html
+    ? L.divIcon(props)
+    : L.icon({
+        iconUrl: defaultIcon,
+        shadowUrl: defaultShadow,
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41],
+        ...props
+      });
 
   return Icon;
 };
@@ -32,4 +36,22 @@ export const getGeoJSONList = (d) => {
     );
   }
   return [d];
+};
+
+export const fnMarker = (
+  latlng,
+  { children = null, label = null, popUp = {}, icon = {}, ...props }
+) => {
+  const m = L.marker(latlng, {
+    icon: MarkerIcon(icon),
+    ...props
+  });
+  if (label) {
+    m.bindPopup(label, popUp || {});
+  }
+  if (children) {
+    const content = renderReactToDiv(children);
+    m.bindPopup(content, popUp || {});
+  }
+  return m;
 };
